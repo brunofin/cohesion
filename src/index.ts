5c50af584a8cf2602b34f7f9fc5d56947111c141
Bruno Finger <bruno.k.finger@gmail.com>
2024-04-11 20:53:52 +0200

    test extension

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ if (!app.requestSingleInstanceLock()) {
     app.quit();
     process.exit();
 } else {
-    app.on('second-instance', (event, commandLine, workingDirectory) => {
+    app.on('second-instance', (event, commandLine) => {
         // Someone tried to run a second instance, we should focus our window.
         if (mainWindow) {
             let url: string = commandLine?.find(arg => arg.startsWith('notion://www.notion.so/'));
@@ -35,6 +35,6 @@ if (!app.requestSingleInstanceLock()) {
     
     app.whenReady().then(() => {
         mainWindow = new Cohesion().init(preloadUrl)
-        console.log('title1', preloadUrl);
+        console.log('title1', preloadUrl, process.execPath, process.argv);
     });
 }
